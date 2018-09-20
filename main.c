81cfdf9686cdbfd0da2cb6e5edc67a69c23e8781
V <v@Vs-MacBook-Pro.local>
2018-09-20 15:24:19 +0700

    Update file...

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int countUnsignedNumber;
+     
+     int arryNumber;
+     printf("Vui lòng nhập size của mảng:\n");
+     int countUnsignedNumber;
+     for(int i = 0; i < arryNumber; i++) {
+         printf("Vui lòng nhập giá trị tại index %d\n", i);
+         scanf("%d", &countUnsignedNumber);
+     }
+     for(int i = 0; i < arryNumber; i++) {
+         printf("Giá trị phần tử tại index %d là : %d", i, )
+     }
